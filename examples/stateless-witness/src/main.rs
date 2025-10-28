9063783101ad16c581af9a26e79b27dc313dc6c2
Ignacio Hagopian <jsign.uy@gmail.com>
2025-10-28 14:40:05 -0300

    with more checks

--- examples/stateless-witness/src/main.rs
+++ examples/stateless-witness/src/main.rs
@@ -1,5 +1,6 @@
 #![warn(unused_crate_dependencies)]
 
+use core::panic;
 use std::sync::Arc;
 
 use alloy_primitives::{keccak256, Bytes, U256};
@@ -8,7 +9,7 @@ use reth_ethereum::{
     chainspec::ChainSpecBuilder,
     evm::{
         primitives::{execute::Executor, ConfigureEvm},
-        revm::{database::StateProviderDatabase, State},
+        revm::{database::StateProviderDatabase, db::AccountState, State},
         EthEvmConfig,
     },
     network::types::BlockHashOrNumber,
@@ -20,17 +21,18 @@ use reth_ethereum::{
 use reth_execution_types::witness::FlatWitnessRecord;
 use reth_stateless::{flat_witness::FlatExecutionWitness, UncompressedPublicKey};
 use reth_storage_api::HeaderProvider;
+use revm::Database;
 
 fn main() -> eyre::Result<()> {
     let datadir = std::env::var("RETH_DATADIR")?;
     let chain_spec = Arc::new(ChainSpecBuilder::mainnet().build());
     let factory = EthereumNode::provider_factory_builder()
-        .open_read_only(chain_spec.clone().into(), ReadOnlyConfig::from_datadir(datadir))?;
+        .open_read_only(chain_spec.clone(), ReadOnlyConfig::from_datadir(datadir))?;
 
     let provider = factory.provider()?;
 
     let block = provider
-        .recovered_block(BlockHashOrNumber::Number(23175593), Default::default())
+        .recovered_block(BlockHashOrNumber::Number(23175588), Default::default())
         .unwrap()
         .unwrap();
 
@@ -40,7 +42,8 @@ fn main() -> eyre::Result<()> {
     println!("Executing block number: {block_number}");
 
     let evm_config = EthEvmConfig::new(chain_spec.clone());
-    let block_executor = evm_config.executor(StateProviderDatabase::new(state_provider));
+    let db = StateProviderDatabase::new(state_provider);
+    let block_executor = evm_config.executor(db);
 
     let mut witness_record = FlatWitnessRecord::default();
     let mut lowest_block_number = Default::default();
@@ -81,7 +84,31 @@ fn main() -> eyre::Result<()> {
         headers.iter().zip(range).map(|(bytes, num)| (U256::from(num), keccak256(bytes))).collect();
 
     let parent = headers.last().unwrap().clone();
-    let witness = FlatExecutionWitness::new(pre_state, block_hashes, parent);
+    let flat_witness = FlatExecutionWitness::new(pre_state, block_hashes, parent);
+
+    let mut db = StateProviderDatabase::new(state_provider);
+    for (address, flatdb_account) in flat_witness.state.accounts.iter() {
+        let trie_account = db.basic(*address).unwrap();
+        let flatdb_account_info = (flatdb_account.account_state != AccountState::NotExisting)
+            .then_some(flatdb_account.clone().info);
+
+        if trie_account != flatdb_account_info {
+            panic!(
+                "Mismatch in account info for address {:?}: trie={:?}, flatdb={:?}",
+                address, trie_account, flatdb_account_info
+            );
+        }
+
+        for (slot, value) in flatdb_account.storage.iter() {
+            let trie_value = db.storage(*address, *slot).unwrap();
+            if trie_value != *value {
+                panic!(
+                    "Mismatch in storage for address {:?} at slot {:?}: trie={:?}, flatdb={:?}",
+                    address, slot, trie_value, value
+                );
+            }
+        }
+    }
 
     let public_keys = recover_signers(block.body().transactions())
         .expect("Failed to recover public keys from transaction signatures");
@@ -89,7 +116,7 @@ fn main() -> eyre::Result<()> {
     reth_stateless::stateless_validation_with_flatdb(
         block.into_block(),
         public_keys,
-        witness,
+        flat_witness,
         chain_spec,
         evm_config,
     )
